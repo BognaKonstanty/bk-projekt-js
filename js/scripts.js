d31c1b38e33d57681789af126662a8559e5b02c5
Bogna Konstanty <rosiakb@gmail.com>
2016-12-21 15:40:48 +0100

    add variables

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+var a= 20;
+var h= 10;
+var triangleArea= a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
